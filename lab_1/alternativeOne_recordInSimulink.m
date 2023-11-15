245eb2a96e788e009982148a3dd006d49f22dbe7
Philip Welin-Berger <philip.welin.berger@gmail.com>
2023-11-15 10:29:54 +0100

    formatting

--- lab_1/alternativeOne_recordInSimulink.m
+++ lab_1/alternativeOne_recordInSimulink.m
@@ -1,6 +1,6 @@
 % This file comes with the simulink diagram recordSounds.mdl
 % It requires a functioning microphone and the DSP systems toolbox.
-% Tested on a Laptop with Matlab R2014a, Windows 7, October 2014. 
+% Tested on a Laptop with Matlab R2014a, Windows 7, October 2014.
 
 % The code records a sound file using simulink.
 % Please run this file section by section!
@@ -14,9 +14,9 @@ open_system('recordSounds'); % opens the simulink diagram
 
 % Some advice:
 % - You can open the "From Audio Device" and adjust the recording device if
-%   necessary. 
+%   necessary.
 % - Start the recording with the play button.
-% - After running the simulink diagram, the variable y will appear in your 
+% - After running the simulink diagram, the variable y will appear in your
 %   workspace. It contains the recorded sound signal.
 % - Make sure that the volume of your recording device is reasonable. You
 %   can listen to your recordings in matlab and also look at a plot of the
@@ -24,14 +24,14 @@ open_system('recordSounds'); % opens the simulink diagram
 
 %% second, we'll listen to our recording
 
-sound(y,fSamp); % make sure that the quality of the recording is okay
+sound(y, fSamp); % make sure that the quality of the recording is okay
 
 %% third, we'll have a look at the data
 
-nSamp = size(y,1); % number of samples
-t = (0:nSamp-1)/fSamp; % time vector in seconds
+nSamp = size(y, 1); % number of samples
+t = (0:nSamp - 1) / fSamp; % time vector in seconds
 
-figure(1);clf();
+figure(1); clf();
 plot(t, y)
 xlabel('time in seconds')
 ylabel('recorded signal') % axis description is important!
--- lab_1/alternativeTwo_readWavFile.m
+++ lab_1/alternativeTwo_readWavFile.m
@@ -1,13 +1,13 @@
 % This file comes with the file recording.wav
-% Tested on a Laptop with Matlab R2014a, Windows 7, November 2014. 
+% Tested on a Laptop with Matlab R2014a, Windows 7, November 2014.
 
 % The code shows how to read in a wav file that you have recorded, for example,
-% on your Android phone. 
+% on your Android phone.
 
 %% first, we'll read in the pre-recorded wave file
 
 % We assume that the wave file has been recorded with a sampling frequency
-% of 8000Hz! 
+% of 8000Hz!
 
 [y, fSamp] = audioread('recording.wav'); % extract data and sampling frequency
 
@@ -21,14 +21,14 @@ size(y) % if your recording is in stereo, you can remove one of the channels
 
 %% second, we'll listen to our recording
 
-sound(y,fSamp); % make sure that the quality of the recording is okay
+sound(y, fSamp); % make sure that the quality of the recording is okay
 
 %% third, we'll have a look at the data
 
-nSamp = size(y,1); % number of samples
-t = (0:nSamp-1)/fSamp; % time vector in seconds
+nSamp = size(y, 1); % number of samples
+t = (0:nSamp - 1) / fSamp; % time vector in seconds
 
-figure(1);clf();
+figure(1); clf();
 plot(t, y)
 xlabel('time in seconds')
 ylabel('recorded signal') % axis description is important!
